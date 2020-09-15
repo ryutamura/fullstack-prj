57bd2a94e55fa2fde003e959d22a0015ffb0a161
ryutamura <rtamura@bluecatnetworks.com>
2020-09-15 11:29:45 +0900

    modified files

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../styles/App.css';
 import Search from "./Search";
 import Info from "./Info"
--- my-app/src/components/Search.jsx
+++ my-app/src/components/Search.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 
 function Search(){
+    useEffect(() => {
+        console.log('useEffect was called!');
+    })
+
     return(
         <p>This is Search component</p>
     );
