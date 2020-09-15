3fd7e967d46d84317a4952d895407c27d5450ab3
ryutamura <rtamura@bluecatnetworks.com>
2020-09-15 19:37:38 +0900

    modified knexfile

--- my-app/knexfile.js
+++ my-app/knexfile.js
@@ -29,18 +29,15 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: 
+      process.env.DATABASE_URL ||
+      'postgres://rtamura@localhost:5432/rappers',
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/server/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/server/db/seeds'
     }
   }
 
