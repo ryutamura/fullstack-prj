bb6f4911e63da8db492edba392168d2e041f61d6
ryutamura <rtamura@bluecatnetworks.com>
2020-09-15 20:57:18 +0900

    modified files

--- my-app/knexfile.js
+++ my-app/knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
   development: {
     client: "pg",
     connection: 
-      process.env.DATABASE_URL ||
+      // process.env.DATABASE_URL ||
       'postgres://rtamura@localhost:5432/rappers',
     migrations: {
       directory: __dirname + '/server/db/migrations',
@@ -19,7 +19,8 @@ module.exports = {
   staging: {
     client: 'pg',
     connection: 
-      'postgres://qyceryminagxde:7462144f3bba8d063b21beb637828240c9a1d33b030d6badf21ff1cf2a1b06b5@ec2-107-20-15-85.compute-1.amazonaws.com:5432/d8c0i56jk48u49',
+      process.env.DATABASE_URL ||
+      'postgres://rtamura@localhost:5432/rappers',
     migrations: {
       directory: __dirname + '/server/db/migrations',
     },
--- my-app/server/app.js
+++ my-app/server/app.js
@@ -1,49 +1,71 @@
 const express = require("express");
 const rapperData = require("../data/data.json")
 const path = require("path");
+const db = require("./knex");
 
 const app = express();
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-app.get('/api/rappers', (_, res) => {
-    // const results = rapperData.map((element) => {
-    //     return element.latitude = element.location_coordinates[0];
-    //     // element.longitude = element.location_coordinates[1];
-    // } )
-    // for (const rapper of rapperData) {
-    //     results.push(rapper.fields);
-    // }
-    let results = [];
-    // for (let i = 0; i < rapperData.length; i++) {
-    //     let rapper = rapperData[i];
-    //     rapper.latitude = rapper.location_coordinates[0];
-    //     rapper.longitude = rapper.location_coordinates[1];
-    //     results.push(rapper);
-    // }
+// app.get('/api/rappers', (_, res) => {
+//     // const results = rapperData.map((element) => {
+//     //     return element.latitude = element.location_coordinates[0];
+//     //     // element.longitude = element.location_coordinates[1];
+//     // } )
+//     // for (const rapper of rapperData) {
+//     //     results.push(rapper.fields);
+//     // }
+//     let results = [];
+//     // for (let i = 0; i < rapperData.length; i++) {
+//     //     let rapper = rapperData[i];
+//     //     rapper.latitude = rapper.location_coordinates[0];
+//     //     rapper.longitude = rapper.location_coordinates[1];
+//     //     results.push(rapper);
+//     // }
 
-    for (let i = 0; i < rapperData.length; i++) {
-        let rapper = rapperData[i];
-        delete rapper.bio_deathdate;
-        delete rapper.bio_yearsactiveend;
-        results.push(rapper);
-    }
-    // console.log('yoyoyo');
-    // console.log(rapperData);
-    res.json(results);
+//     for (let i = 0; i < rapperData.length; i++) {
+//         let rapper = rapperData[i];
+//         delete rapper.bio_deathdate;
+//         delete rapper.bio_yearsactiveend;
+//         results.push(rapper);
+//     }
+//     // console.log('yoyoyo');
+//     // console.log(rapperData);
+//     res.json(results);
     
-})
+// })
 
-app.get('/api/rappers/:name', (req, res) => {
-    const name = req.params.name;
-    for (let i = 0; i < rapperData.length; i++) {
-        const rapper = rapperData[i]; 
-        if (rapper.fields.name === name) {
-            res.json(rapper);
-        }
+app.get('/api/rappers', async (req, res) => {
+    try{
+        const rapper = await db
+        .select()
+        .from('rapper_info');
+        console.log(rapper[0]);
+        res.json(rapper);
+    } catch (err) {
+        console.log('error')
+        res.sendStatus(500);
     }
-})
+});
+
+app.get("/api/rappers/:name", async (req, res) => {
+    try{
+        const name = req.params.name;
+        console.log(name);
+        const rapper = await db
+        .select()
+        .where({
+            name: name
+        })
+        .from("rapper_info");
+        console.log(`rapper is:${rapper[0].name}`);
+        res.json(rapper);
+    } catch (err) {
+        console.log('error');
+        res.sendStatus(500);
+    }
+});
 
 app.get('/api/rappers/location/:name', (req, res) => {
     const name = req.params.name;
