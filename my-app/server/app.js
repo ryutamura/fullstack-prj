e5d318e074780949c4a665f9065771ae0bbb9c8a
ryutamura <rtamura@bluecatnetworks.com>
2020-09-16 11:56:27 +0900

    modified api endpoint logic for backend connection

--- my-app/server/app.js
+++ my-app/server/app.js
@@ -8,43 +8,14 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
-// app.get('/api/rappers', (_, res) => {
-//     // const results = rapperData.map((element) => {
-//     //     return element.latitude = element.location_coordinates[0];
-//     //     // element.longitude = element.location_coordinates[1];
-//     // } )
-//     // for (const rapper of rapperData) {
-//     //     results.push(rapper.fields);
-//     // }
-//     let results = [];
-//     // for (let i = 0; i < rapperData.length; i++) {
-//     //     let rapper = rapperData[i];
-//     //     rapper.latitude = rapper.location_coordinates[0];
-//     //     rapper.longitude = rapper.location_coordinates[1];
-//     //     results.push(rapper);
-//     // }
 
-//     for (let i = 0; i < rapperData.length; i++) {
-//         let rapper = rapperData[i];
-//         delete rapper.bio_deathdate;
-//         delete rapper.bio_yearsactiveend;
-//         results.push(rapper);
-//     }
-//     // console.log('yoyoyo');
-//     // console.log(rapperData);
-//     res.json(results);
-    
-// })
-
-app.get('/api/rappers', async (req, res) => {
+app.get('/api/rappers', async (_, res) => {
     try{
         const rapper = await db
         .select()
         .from('rapper_info');
-        console.log(rapper[0]);
         res.json(rapper);
     } catch (err) {
-        console.log('error')
         res.sendStatus(500);
     }
 });
@@ -59,22 +30,12 @@ app.get("/api/rappers/:name", async (req, res) => {
             name: name
         })
         .from("rapper_info");
-        console.log(`rapper is:${rapper[0].name}`);
-        res.json(rapper);
+        res.json(rapper[0]);
     } catch (err) {
         console.log('error');
         res.sendStatus(500);
     }
 });
 
-app.get('/api/rappers/location/:name', (req, res) => {
-    const name = req.params.name;
-    for (let i = 0; i < rapperData.length; i++) {
-        const rapper = rapperData[i]; 
-        if (rapper.fields.name === name) {
-            res.json(rapper.fields.location_coordinates);
-        }
-    }
-})
 
 module.exports = app;
